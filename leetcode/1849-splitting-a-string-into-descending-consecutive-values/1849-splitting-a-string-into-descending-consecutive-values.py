36d0aaf480956483d4e93fb626e5d408fae3dc22
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-04-12 02:57:41 -0400

    Add solution for Splitting a String Into Descending Consecutive Values

--- leetcode/1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ leetcode/1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -4,6 +4,8 @@ class Solution:
         ans = False
         def backtrack(string, path):
             nonlocal ans
+            if ans:
+                return
             if not string:
                 for i in range(len(path) - 1):
                     if int(path[i]) != int(path[i+1]) + 1:
