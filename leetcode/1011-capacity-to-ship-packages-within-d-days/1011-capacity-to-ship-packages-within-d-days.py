3b85c07d9d24d924790ccdc76d3921c3457bd9ed
Abdualziz <149840268+abdulaziz-moh@users.noreply.github.com>
2026-04-16 16:40:22 +0300

    Add solution for Capacity To Ship Packages Within D Days

--- leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ leetcode/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,75 +1,24 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
 
-
-            def check(mid):
-                count, sm = 0, 0
+            def calc_days(w):
+                sm, cnt = 0, 0
                 for v in weights:
                     sm += v
-                    if sm > mid:
-                        count += 1
+                    if sm > w:
+                        cnt += 1
                         sm = v
-                count += 1
+                if sm > 0:
+                    cnt += 1
+                return cnt
 
-                if count <= days:
-                    return True
-                return False
+            left , right = max(weights), sum(weights)
 
-            left, right = max(weights), sum(weights)
             while left <= right:
                 mid = (left + right) // 2
-                if check(mid):
+
+                if calc_days(mid) <= days:
                     right = mid - 1
                 else:
                     left = mid + 1
             return right + 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        # def check_valid_capacity(num):
-        #     count = 0
-        #     sum = 0
-        #     for v in weights:
-        #         sum += v
-        #         status = True
-        #         if sum == num:
-        #             count += 1
-        #             sum = 0
-        #         elif sum > num:
-        #             count += 1
-        #             sum = v
-        #             status = False
-
-        #         else:
-        #             status = False
-        #     if not status:
-        #         count += 1
-        #     if count <= days:
-        #         return True
-        #     return False
-
-        # left, right = max(weights), sum(weights)
-        # while left <= right:
-        #     mid = (left + right) // 2
-        #     # print(left, " ", mid, " ", right)
-        #     if check_valid_capacity(mid):
-        #         right = mid - 1
-        #     else:
-        #         left = mid + 1
-        # return right + 1
-
-        
